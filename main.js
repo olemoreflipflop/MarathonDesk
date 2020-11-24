f562f5d10e0af87100805dc2e5a3f76a2789fb91
olemoreflipflop <47281172+olemoreflipflop@users.noreply.github.com>
2020-11-24 21:02:06 +0300

    HW-4

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 /*buttons on control field*/
 const $kick = $getElById('btn-kick');
+const $punch = $getElById('btn-punch');
 const $start = $getElById('btn-start');
 const $fatality = $getElById('btn-fatality');
 
@@ -8,126 +9,166 @@ let characterDamage = 0;
 let enemyDamage = 0;
 
 const $logs = document.querySelector('#logs');
+const maxPunchClicks = 5;
+const maxKickClicks = 20;
+/*const kickButtonInner = $kick.innerText;
+const punchButtonInner = $punch.innerText;*/
 
 const character = {
-	name: 'Pikachu',
-	defaultHP: 150,
-	damageHP: 150,
-	elHP: $getElById('health-character'),
-	elProgressbar: $getElById('progressbar-character'),
-	changeHP: changeHP,
-	renewHP: renewHP,
-	renderHP: renderHP,
-	renderTextHP: renderTextHP,
-	renderProgressbar: renderProgressbar,
-	showLog: showLog,
+    name: 'Pikachu',
+    defaultHP: 150,
+    damageHP: 150,
+    elHP: $getElById('health-character'),
+    elProgressbar: $getElById('progressbar-character'),
+    changeHP,
+    renewHP,
+    renderHP,
+    renderTextHP,
+    renderProgressbar,
+    showLog,
 }
 
 const enemy = {
-	name: 'Charmander',
-	defaultHP: 150,
-	damageHP: 150,
-	elHP: $getElById('health-enemy'),
-	elProgressbar: $getElById('progressbar-enemy'),
-	changeHP: changeHP,
-	renewHP: renewHP,
-	renderHP: renderHP,
-	renderTextHP: renderTextHP,
-	renderProgressbar: renderProgressbar,
-	finalAttack: finalAttack,
-	showLog: showLog,
+    name: 'Charmander',
+    defaultHP: 150,
+    damageHP: 150,
+    elHP: $getElById('health-enemy'),
+    elProgressbar: $getElById('progressbar-enemy'),
+    changeHP,
+    renewHP,
+    renderHP,
+    renderTextHP,
+    renderProgressbar,
+    finalAttack,
+    showLog,
 }
 
 function $getElById(id) {
-	return document.getElementById(id);
+    return document.getElementById(id);
 }
 
+const countKick = countClicks($kick, maxKickClicks);
+
 $kick.addEventListener('click', function() {
-	characterDamage = random(15);
-	enemyDamage = random(15);
-	character.changeHP(enemyDamage);
-	enemy.changeHP(characterDamage);
-	if((character.damageHP > enemy.damageHP) && (enemy.damageHP <= 10 && enemy.damageHP > 0)) {
-		showButton($fatality);
-	}
+    console.log('Kick');
+    countKick();
+    characterDamage = random(15);
+    enemyDamage = random(15);
+    character.changeHP(enemyDamage);
+    enemy.changeHP(characterDamage);
+    if((character.damageHP > enemy.damageHP) && (enemy.damageHP <= 10 && enemy.damageHP > 0)) {
+        showButton($fatality);
+    }
+});
+
+const countPunch = countClicks($punch, maxPunchClicks);
+
+$punch.addEventListener('click', function() {
+    console.log('Punch');
+    countPunch();
+    characterDamage = random(20);
+    enemyDamage = random(20);
+    character.changeHP(enemyDamage);
+    enemy.changeHP(characterDamage);
+    if((character.damageHP > enemy.damageHP) && (enemy.damageHP <= 10 && enemy.damageHP > 0)) {
+        showButton($fatality);
+    }
 });
 
 $fatality.addEventListener('click', function() {
-	enemy.finalAttack();
+    enemy.finalAttack();
 });
+
 
 $start.addEventListener('click', function() {
-	showButton($kick);
-	hideButton($start);
-	enemy.renewHP();
-	character.renewHP();
-	$logs.innerHTML = '';
+    showButton($kick);
+    showButton($punch);
+    hideButton($start);
+    enemy.renewHP();
+    character.renewHP();
+    $logs.innerHTML = '';
 });
 
+function countClicks(button, counter = 5) {
+    const innerText = button.innerText;
+    button.innerText = `${innerText} (${counter})`;
+    return function () {
+        counter--;
+        if (counter === 0) {
+            button.disabled = true;
+        }
+        console.log(button.innerText);
+        button.innerText = `${innerText} (${counter})`;
+        return counter;
+    }
+}
+
 function showButton(button) {
-	button.style.removeProperty('display');
+    button.style.removeProperty('display');
 }
 
 function hideButton(button) {
-	button.style.display = "none";
+    button.style.display = "none";
 }
 
 function random(num) {
- 	return Math.ceil(Math.random() * num );
+     return Math.ceil(Math.random() * num );
 }
 
 function init() {
-	character.renderHP();
-	enemy.renderHP(); 
-	hideButton($kick);
-	hideButton($fatality);
+    character.renderHP();
+    enemy.renderHP(); 
+    hideButton($kick);
+    hideButton($punch);
+    hideButton($fatality);
 }
 
 init();
 
 function renewHP() {
-	this.damageHP = this.defaultHP;
-	this.renderHP();
+    this.damageHP = this.defaultHP;
+    this.renderHP();
 }
 
 function renderHP() {
-	this.renderTextHP();
-	this.renderProgressbar();
+    this.renderTextHP();
+    this.renderProgressbar();
 }
 
 function renderTextHP() {
-	this.elHP.innerText = this.damageHP + ' / ' + this.defaultHP;
+    this.elHP.innerText = this.damageHP + ' / ' + this.defaultHP;
 }
 
 function renderProgressbar() {
-	const health = Math.ceil((this.damageHP * 100) / this.defaultHP);
-	this.elProgressbar.style.width = health + '%';
+    const health = Math.ceil((this.damageHP * 100) / this.defaultHP);
+    this.elProgressbar.style.width = health + '%';
 }
 
 function finalAttack() {
-	this.changeHP(this.damageHP);
+    this.changeHP(this.damageHP);
 }
 
 function changeHP(attack, player) {
-	this.damageHP -= attack;	
-	
-	if(this.damageHP <= 0) {
-		this.damageHP = 0;
-		alert(`${this.name} проиграл.` );
-		hideButton($kick);
-		hideButton($fatality);
-		showButton($start);
-	}
+    this.damageHP -= attack;    
+    
+    if(this.damageHP <= 0) {
+        this.damageHP = 0;
+        alert(`${this.name} проиграл.` );
+        hideButton($kick);
+        hideButton($fatality);
+        hideButton($punch);
+        showButton($start);
+    }
 
-	this.renderHP();
-	this.showLog();
+    this.renderHP();
+    this.showLog();
 }
 
 function generateLog(firstPlayer, secondPlayer, attackDamage) {
-	const {name, elHP } = firstPlayer;
-	const {name: enemyName} = secondPlayer;
+    const {name, elHP } = firstPlayer;
+    const {name: enemyName} = secondPlayer;
 
-	const logs = [
+    const logs = [
     `${name} вспомнил что-то важное, но неожиданно ${enemyName}, не помня себя от испуга, ударил в предплечье врага. ${name} получил -${attackDamage} урона, [${elHP.innerText}]`,
     `${name} поперхнулся, и за это ${enemyName} с испугу приложил прямой удар коленом в лоб врага. ${name} получил -${attackDamage} урона, [${elHP.innerText}]`,
     `${name} забылся, но в это время наглый ${enemyName}, приняв волевое решение, неслышно подойдя сзади, ударил. ${name} получил -${attackDamage} урона, [${elHP.innerText}]`,
@@ -138,9 +179,9 @@ function generateLog(firstPlayer, secondPlayer, attackDamage) {
     `${name} расстроился, как вдруг, неожиданно ${enemyName} случайно влепил стопой в живот соперника. ${name} получил -${attackDamage} урона, [${elHP.innerText}]`,
     `${name} пошатнулся, и внезапно наглый ${enemyName} беспричинно ударил в ногу противника. ${name} получил -${attackDamage} урона, [${elHP.innerText}]`,
     `${name} пытался что-то сказать, но вдруг, неожиданно ${enemyName} со скуки, разбил бровь сопернику. ${name} получил -${attackDamage} урона, [${elHP.innerText}]`
-	];
+    ];
 
-	return logs[random(logs.length) - 1];
+    return logs[random(logs.length) - 1];
 }
 
 function showLog (firstPlayer, secondPlayer, attackDamage) {
